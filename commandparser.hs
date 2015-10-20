4ff3207d456aeda4c9bf271ba515979e152f6c63
Michael Spallino <mspallino@wit.edu>
2015-10-20 10:29:25 -0400

    You can now look at locations.

--- commandparser.hs
+++ commandparser.hs
@@ -1,6 +1,12 @@
 import Text.ParserCombinators.Parsec
 import Data.Typeable
+import Data.List
 import qualified Data.Map as Map
+
+
+--
+-- Parser for player input
+--
 
 -- Each line contains 1 or more cells, separated by a space
 line :: GenParser Char st [String]
@@ -31,7 +37,9 @@ cellContent =
 parseText :: String -> Either ParseError [String]
 parseText input = parse line "(unknown)" input
 
+--
 -- HERE BE GAME STUFFS
+--
 
 -- Valid Command data structure set
 data ValidCommand = Look | Take | Use | Move | Talk | INVALID_COMMAND deriving (Show)
@@ -48,7 +56,25 @@ verifyCommand cmd = do
         _      -> INVALID_COMMAND
 
 -- Parse user input and verify the first element is a valid command.
-getCommand :: String -> ValidCommand
+getCommand :: String -> Maybe String
 getCommand str = do
-    verifyCommand (either (\_ -> "invalid") (head) (parseText str))
+    let input = parseText str
+    let cmd = verifyCommand (either (\_ -> "invalid") (head) (input))
+    let params = either (\_ -> ["invalid"]) (tail) (input)
+    case cmd of
+        Look -> if Map.member (concat params) locations then Map.lookup (concat params) locations else Just "The Metallic Atomic Mayo universe doesn't have that place." 
+        Take -> Just "TAKEN."
+        Use  -> Just "USED."
+        Move -> Just "MOVED."
+        Talk -> Just "TALKED."
+        INVALID_COMMAND -> Just "Invalid input provided."
 
+locations = Map.fromList [("HeinsVille", "Main city, oddly resembles a ketchup bottle..."), 
+                          ("NuclearReactor", "What do you think a nuclear reactor looks like?"),
+                          ("BigMacBay", "A heart attack waiting to happen."),
+                          ("HiddenValley","Yes like the dressing, idiot."),
+                          ("DillCity", "The pickle came from here."), 
+                          ("CBSHeadquarters", "TV news station. Left of mayo affiliation."),
+                          ("Saltropolis", "Who doesn't love salt?"),
+                          ("Cobbler's main office", "There ain't even a window."),
+                          ("Sugaria", "I have no idea what this place is for yet.")]
