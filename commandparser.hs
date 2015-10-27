d936ef5e92b745331412b0fad868c8e09cddcfae
Michael Spallino <mspallino@wit.edu>
2015-10-27 14:29:39 -0400

    Help command and story intro.

--- commandparser.hs
+++ commandparser.hs
@@ -54,7 +54,7 @@ data PlayerCommand = PlayerCommand {
 } deriving (Show)
 
 -- Valid Command data structure set
-data ValidCommand = Look | Take | Use | Move | Talk | Items | Inventory | INVALID_COMMAND deriving (Show)
+data ValidCommand = Look | Take | Use | Move | Talk | Items | Inventory | Help | INVALID_COMMAND deriving (Show)
 
 -- Take a string and return a Valid Command
 verifyCommand :: String -> ValidCommand
@@ -67,6 +67,7 @@ verifyCommand cmd = do
         "talk"      -> Talk
         "items"     -> Items
         "inventory" -> Inventory
+        "help"      -> Help
         _           -> INVALID_COMMAND
 
 -- Parse user input and verify the first element is a valid command.
@@ -95,6 +96,7 @@ getCommand plyCmd = do
                     (PlayerCommand (player plyCmd) (inputStr plyCmd) ("Invalid location provided."))
         Items -> (PlayerCommand (player plyCmd) (inputStr plyCmd) (Data.List.intercalate ", " (fromJust (Map.lookup curloc items))))
         Inventory -> (PlayerCommand (player plyCmd) (inputStr plyCmd) (Data.List.intercalate ", " (inventory (player plyCmd))))
+        Help -> (PlayerCommand (player plyCmd) (inputStr plyCmd) helpStr)
         INVALID_COMMAND -> (PlayerCommand (player plyCmd) (inputStr plyCmd) ("Invalid command provided."))
 
 
@@ -106,6 +108,8 @@ locations = Map.fromList [("HeinsVille", Map.fromList [("NuclearReactor", "What 
 
 items = Map.fromList [("HeinsVille", ["food", "dog"])]
 
+helpStr = "\nValid Commands:\n\tlook <LOCATION>\n\ttake <ITEM_NAME>\n\tuse <ITEM_NAME>\n\tmove <LOCATION>\n\titems\n\tinventory\n\thelp"
+
 
 play p1 plyCmd = do
     let resCmd = getCommand plyCmd
@@ -116,7 +120,7 @@ play p1 plyCmd = do
     play p1 newCmd
 
 main = do
-    putStrLn "\nWelcome to Metallic Atomic Mayo!\n\nValid Commands:\n\n\tlook <LOCATION>\n\ttake <ITEM_NAME>\n\tuse <ITEM_NAME>\n\tmove <LOCATION>\n\titems\n\tinventory\n\nHave fun!"
+    putStrLn "Valid Commands:\n\tlook <LOCATION>\n\ttake <ITEM_NAME>\n\tuse <ITEM_NAME>\n\tmove <LOCATION>\n\titems\n\tinventory\n\t\t\tCBS NEWS: **MAYO STOLEN!**\n\t\"Sandman, who works at the Nuclear Reactor here in HeinsVille, had his   famous Metallic Atomic Mayo stolen! For those unaware, Sandman eats a sandwhich everyday at work and recently he knocked his jar of mayo into the reactor       creating Metallic Atomic Mayo!\n\tBecause Metallic Atomic Mayo doesn't decay, this became a very lucrative business. Sandman made millions off of his expiration date free product. For   obvious reasons he had to be protective of this amazing mayonnaise. Alas, his   protection contracted from the Secret Service was not enough. The mayo was      stolen by the most notorious villain in HeinsVille, Rueben.\"\n\nYou are a Private Investigator hired to find the MAM. You’ve been living in     HeinsVille for the last 5 years and have been very good friends with Sandman    since you moved here and that is why he contacted you.\n\nYou MUST find that MAYO. If you forget the commands, type \"help\"."
     putStr "-> "
     theCommand <- getLine
     let p1 = Player "HeinsVille" ["note"]
